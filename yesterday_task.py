3409c360da02d96eab937e18d65caf53234a88e0
Mukesh Patil <mail.mukeshpatil@gmail.com>
2024-11-08 10:16:23 +0530

    Delete yesterday_task.py

--- yesterday_task.py
+++
@@ -1,9 +0,0 @@
-def yesterday_task():
-    if os.path.isfile(yesterday) or os.path.getsize(today):
-        with open(yesterday, 'r') as file:
-            yesterday_tasks = file.readlines()
-        with open(today, 'a') as file:
-            for line in yesterday_tasks:
-                task_name, status = line.split(',')
-                if status.strip() == "Not done":
-                    file.write(f'{task_name},Not done\n')
